a885ba5e7018a7f95340483192ddf0ae7c9d969d
KimJeongHoon <hedgehoon@gmail.com>
2023-09-01 17:15:32 +0900

    fgf

--- database/DC18/dock/java17/docker-entrypoint.sh
+++ database/DC18/dock/java17/docker-entrypoint.sh
@@ -1,9 +1,12 @@
-#! /bin/bash
+#!/bin/bash
 
-USERNAME=${USERNAME:-user}
-PASSWORD=${PASSWORD:-user}
+USERNAME=${USERNAME=-user}
+PASSWORD=${PASSWORD=-user}
+GIT_URL=${GIT_URL}
 
 useradd -s /bin/bash -m $USERNAME
 echo "$USERNAME:$PASSWORD" | chpasswd
+
+su $USERNAME -c "cd ~/ && git clone $GIT_URL"
 
 /usr/sbin/sshd -D -e
